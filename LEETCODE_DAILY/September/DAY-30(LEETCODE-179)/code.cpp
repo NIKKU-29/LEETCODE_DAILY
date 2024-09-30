d5728bf05fe3669ca9f8ab08894cda587f5d4d6a
NIKKU-29 <113784295+NIKKU-29@users.noreply.github.com>
2024-10-01 01:58:24 +0530

    Add files via upload

---
+++ LEETCODE_DAILY/September/DAY-30(LEETCODE-179)/code.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class CustomStack {
+
+    deque<int>dq;
+    int maxSize;
+
+public:
+    CustomStack(int maxSize) : maxSize(maxSize){}
+    
+    void push(int x) {
+
+        if(dq.size()<maxSize) dq.push_front(x);
+        
+    }
+    
+    int pop() {
+
+        if(dq.size()!=0)
+        {
+            int top=dq.front();
+            dq.pop_front();
+            return top;
+        }
+
+        return -1;
+        
+    }
+    
+     void increment(int k, int val) {
+        int limit = min(k, (int)dq.size()); 
+        for (int i = dq.size()-limit; i < dq.size() ; i++) {        
+            dq[i] += val;
+        }
+        
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
